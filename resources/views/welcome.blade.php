c9ecdc1215c9e0deb318e25a21f8735510bfe197
zlegend-210 <yamen.a.abdulrahman@hotmail.com>
2017-09-10 11:08:19 +0200

    test webhook

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>Bus tracking</title>
+    <title>Web hook</title>
 
-	<!-- CSRF Token -->
-	<meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-	<!-- Fonts -->
-	<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
-	<link href="https://fonts.googleapis.com/css?family=Kumar+One" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    <link href="https://fonts.googleapis.com/css?family=Kumar+One" rel="stylesheet">
 </head>
+
 <body>
-	@include('layouts.navbar')
-	<div class="container-fluid" id="app" style="padding-top: 65px;">
-		<h3>Bus system locator</h3>
-	</div>
-	<script src="{{ asset('js/app.js') }}"></script>
+    @include('layouts.navbar')
+    <div class="container-fluid" id="app" style="padding-top: 65px;">
+        <h3>Bus system locator</h3>
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
+
 </html>
