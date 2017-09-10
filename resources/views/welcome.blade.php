03122a325ce932fc37f8fb43dcbf8cf52d1dc80c
zlegend-210 <yamen.a.abdulrahman@hotmail.com>
2017-09-10 11:09:07 +0200

    test webhook

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Web hook</title>
+    <title>Bus tracking</title>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
