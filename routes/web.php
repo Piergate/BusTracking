1be65e191995891e8450e42488c2c59cb91dbe6b
zlegend-210 <yamen.a.abdulrahman@hotmail.com>
2017-09-11 12:40:38 +0200

    live

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,75 +18,67 @@ use Illuminate\Support\Facades\Input;
 
 
 Route::get('/', function () {
-	return view('welcome');
+    return view('welcome');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['middleware' => ['web','auth','role:Admin']], function (){ 
+Route::group(['middleware' => ['web','auth','role:Admin']], function () {
 
-	Route::resource('/lines', 'LineController');
-	Route::get('exportLine/{type}', 'LineController@downloadExcel');
-	Route::resource('/buses', 'BusController');
-	Route::resource('/drivers', 'DriverController');
-	Route::resource('/students', 'StudentController');
-	Route::resource('/supervisors', 'SupervisorController');
-	Route::resource('/trips', 'TripController'); 
-	Route::resource('/manageusers', 'AdminManageUsersController');
-	
+    Route::resource('/lines', 'LineController');
+    Route::get('exportLine/{type}', 'LineController@downloadExcel');
+    Route::resource('/buses', 'BusController');
+    Route::resource('/drivers', 'DriverController');
+    Route::resource('/students', 'StudentController');
+    Route::resource('/supervisors', 'SupervisorController');
+    Route::resource('/trips', 'TripController');
+    Route::resource('/manageusers', 'AdminManageUsersController');
 });
 
-Route::group(['middleware' => ['web','auth','role:Admin|Supervisor']], function (){ 
-	// laravel Excel
-	Route::get('natege', 'NategaController@Natega');
-	Route::get('downloadExcel/{type}', 'NategaController@downloadExcel');
-	Route::post('importExcel', 'NategaController@importExcel');
-
+Route::group(['middleware' => ['web','auth','role:Admin|Supervisor']], function () {
+    // laravel Excel
+    Route::get('natege', 'NategaController@Natega');
+    Route::get('downloadExcel/{type}', 'NategaController@downloadExcel');
+    Route::post('importExcel', 'NategaController@importExcel');
 });
 
 
-Route::group(['middleware' => ['web','auth','role:Driver']], function (){
+Route::group(['middleware' => ['web','auth','role:Driver']], function () {
 
-	Route::get('/startTrip',function(){
-		return view('drivers.position');
-	});
-	Route::post('/save_position',function (Request $request){
+    Route::get('/startTrip', function () {
+        return view('drivers.position');
+    });
+    Route::post('/save_position', function (Request $request) {
 
-		// $last_position = Auth::user()->distinations()->latest()->take(1)->first();
-		// if (isset($last_position)) {
-		// 	if ($last_position->latitude == Input::get('latitude') && $last_position->longitude ==  Input::get('longitude'))
-		// 	{
-		// 		$last_position->update(['updated_at'=>new DateTime()]);
-		// 		$last_position = Auth::user()->distinations()->latest()->take(1)->first();
-		// 	}
+        $last_position = Auth::user()->distinations()->latest()->take(1)->first();
+        if (isset($last_position)) {
+            if ($last_position->latitude == Input::get('latitude') && $last_position->longitude ==  Input::get('longitude')) {
+                $last_position->update(['updated_at'=>new DateTime()]);
+                $last_position = Auth::user()->distinations()->latest()->take(1)->first();
+            }
 
-		// 		if (isset($last_position)) {
-		// 			if ($last_position->latitude == Input::get('latitude') && $last_position->longitude ==  Input::get('longitude'))
-		// 			{
-		// 				$last_position->update(['updated_at' => Carbon::now()]);
+            if (isset($last_position)) {
+                if ($last_position->latitude == Input::get('latitude') && $last_position->longitude ==  Input::get('longitude')) {
+                    $last_position->update(['updated_at' => Carbon::now()]);
+                }
+            } else {
+                $position = Auth::user()->distinations()->create(
+                    $request->only('latitude', 'longitude')
+                    );
+            }
 
-		// 			}
-		// 		}
-		// 		else
-		// 		{
-		// 			$position = Auth::user()->distinations()->create(
-		// 				$request->only('latitude', 'longitude')
-		// 				);
-		// 		}
-
-		// 		return $last_position;
-	});
-	Route::get('/endTrip',function()
-	{
-		$tripUser = Auth::user()->distinations()->get();
-		return view('testTrip',compact('tripUser'));
-	});
-
+            return $last_position;
+        }
+    });
+    Route::get('/endTrip', function () {
+        $tripUser = Auth::user()->distinations()->get();
+        return view('testTrip', compact('tripUser'));
+    });
 });
 
-Route::get('/locatemybus','LocationController@locateMyBus');
+Route::get('/locatemybus', 'LocationController@locateMyBus');
 
 // Route::group(['middleware' => ['web','auth','role:Parent']], function() { //error
 // 	Route::get('/locatemybus', 'LocationController@locateMyBus');
